d523a7810cbe13fd13fe9d7761381112e13cee30
Davi Cajucaru <devcaju@gmail.com>
2022-06-22 20:29:49 -0300

    inital commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "../../node_modules/next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  );
+}
