78d99d1b75a494977c76e4ccbf24273e4b41832b
Davi Cajucaru <devcaju@gmail.com>
2022-07-11 21:25:08 -0300

    feat: add prismic api

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,46 @@
+import Head from "next/head";
+import styles from "./styles.module.scss";
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="#">
+            <time>12 of march of 2022</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages and with shared build, test, and release
+              process.
+            </p>
+          </a>
+
+          <a href="#">
+            <time>12 of march of 2022</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages and with shared build, test, and release
+              process.
+            </p>
+          </a>
+
+          <a href="#">
+            <time>12 of march of 2022</time>
+            <strong>Creating a Monorepo with Lerna & Yarn Workspaces</strong>
+            <p>
+              In this guide, you will learn how to create a Monorepo to manage
+              multiple packages and with shared build, test, and release
+              process.
+            </p>
+          </a>
+        </div>
+      </main>
+    </>
+  );
+}
