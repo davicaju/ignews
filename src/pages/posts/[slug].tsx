b2ea159f8cfd84c983c27e5464f90242bc324109
Davi Cajucaru <devcaju@gmail.com>
2022-07-26 22:18:49 -0300

    feat: add preview page

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -45,9 +45,7 @@ export const getServerSideProps: GetServerSideProps = async ({
 
   const { slug } = params;
 
-  console.log(session);
-
-  if (!session.activeSubscription) {
+  if (!session?.activeSubscription) {
     return {
       redirect: {
         destination: "/",
@@ -78,5 +76,6 @@ export const getServerSideProps: GetServerSideProps = async ({
     props: {
       post,
     },
+    redirect: 60 * 30,
   };
 };
